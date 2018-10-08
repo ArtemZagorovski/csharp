711d53c274c94818f6a935c4dd5ea0b68c79b774
ArtemZagorovski <artemzagorovski@mail.ru>
2018-10-08 22:33:59 +0300

    Add files via upload

---
+++ QuickSortTests/NUnitTestClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+namespace SortingTests
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()
+             public class Tests
+        {
+            [Test]
+            public void Quicksort_RegularIntArrayPassed_ArrayIsSorted()
+            {
+                int[] source = new[] { 3, 1, 5, 4, 9, 6, 2, 7, 8 };
+
+                int p = 0;
+
+                int r = source.Length - 1;
+
+                int[] expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+                Sorting.Quicksort(source, p, r);
+
+                Assert.AreEqual(expected, source);
+            }
+        }
+    }
+}
---
+++ QuickSortTests/QuickSortTests/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class QuickSort
+{
+    static void Sort(int[] arr, int l, int r)
+    {
+        int i = l;
+        int j = r;
+        int x = arr[(l + r) / 2];
+        while (i <= j)
+        {
+            while (arr[i] < x) i++;
+            while (arr[j] > x) j--;
+            if (i <= j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (l < j) Sort(arr, l, j);
+        if (i < r) Sort(arr, i, r);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = new int[11];
+        Random rand = new Random();
+        for (int i = 0; i < arr.Length; i++)
+            arr[i] = rand.Next(-100, 100);
+        foreach (int ar in arr)
+            Console.WriteLine(ar + " ");
+        Console.WriteLine("Сортировка");
+        Sort(arr, 0, arr.Length - 1);
+        foreach (int ar in arr)
+            Console.WriteLine(ar + " ");
+        Console.ReadLine();
+    }
+}
